02369e43ec28961e3a4deb30cd588bbe9810c07f
Grzegorz Kowalski <grzegorzkowalski@yahoo.com>
2024-05-21 11:01:46 +0200

    color box

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,33 +1,41 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import artist from './data/artist.ts';
+import fruits from "./data/fruits.ts";
+import {JSX} from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+    const numA : string | null = prompt("Podaj liczbę A?");
+    const numB : string | null = prompt("Podaj liczbę B?");
+    const color : string | null = prompt("Wybierz kolor (red, green, blue)?");
+    const imageUrl : string = "https://cdn.pixabay.com/photo/2012/11/02/13/02/car-63930_960_720.jpg";
+    const returnBox = (color : string) : JSX.Element => {
+        const properColor : boolean = ["red", "green", "blue"].includes(color);
+        const style : {} = {
+            width: "100px",
+            height: "100px",
+            borderWidth: "5px",
+            borderStyle: "solid",
+            borderColor: color
+        }
 
-  return (
+        return properColor
+            ? <div style={style} />
+            : <div>Nieprawidłowy kolor</div>
+    };
+
+    return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>{Number(numA) + Number(numB)}</h1>
+      <ul>
+          <li>{artist.nickname}</li>
+          <li>{artist.firstName}</li>
+          <li>{artist.age}</li>
+          <li>{artist.lastName}</li>
+      </ul>
+      <p>{fruits.length}</p>
+      <p>{fruits.join("-")}</p>
+      <img src={imageUrl} style={{height: "150px"}} />
+      {returnBox(color!)}
     </>
   )
 }
---
+++ my-app/src/data/artist.ts
@@ -0,0 +1,13 @@
+const artist: {
+    nickname: string,
+    firstName: string,
+    lastName: string,
+    age: number
+} = {
+    nickname: "Eminem",
+    firstName: "Marshall",
+    lastName: "Mathers",
+    age: 49
+};
+
+export default artist;
